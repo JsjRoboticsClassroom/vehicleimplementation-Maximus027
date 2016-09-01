650d22b4546adb39b7b6021cc132c4cfeeb5f1d5
Max <max.a.rosado@gmail.com>
2016-08-31 21:59:09 -0400

    Update Bicycle.java
    Adding to exercise what I have typed. Having issues adding through terminal. Only updated Bicycle.

--- src/nyc/c4q/abassawo/Bicycle.java
+++ src/nyc/c4q/abassawo/Bicycle.java
@@ -5,28 +5,29 @@ package nyc.c4q.abassawo;
  */
 public class Bicycle implements Vehicle, WheeliaAble {
     double speed;
+    double maxSpeed;
 
     public Bicycle(){
         this.speed = 0;
     }
 
 
-    @Override
     public void applyBrakes() {
-
+        speed = 0;
     }
 
-    @Override
-    public void speedUp(int delta) {
-
+    public void speedUp(int speed) {
+        if (speed <= maxSpeed) {
+            speed++;
+        }
     }
 
-    @Override
-    public void slowDown(int delta) {
-
+    public void slowDown(int speed) {
+        if (speed > 0) {
+            speed--;
+        }
     }
 
-    @Override
     public boolean popAWheelie(int duration) {
         while(duration != 0){
             System.out.println("I'm poppping a wheeeelieeee");
